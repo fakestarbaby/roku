9daafff02dcf540c792013f4b78e025be1b3ecbc
fakestarbaby <fakestarbaby@gmail.com>
2016-12-07 08:48:08 +0900

    Initialize project configuration

--- config.lua
+++ config.lua
@@ -1,8 +1,16 @@
+local aspectRatio = display.pixelHeight / display.pixelWidth
+
 application =  {
   content = {
-    width = 320,
-    height = 480,
-    scale = "letterBox",
-    fps = 30,
+    width  = 1.5 < aspectRatio and 320 or 480 / aspectRatio,
+    height = aspectRatio < 1.5 and 480 or 320 * aspectRatio,
+    fps    = 60,
+    scale  = "letterBox",
+    xAlign = "center",
+    yAlign = "center",
+    imageSuffix = {
+      ["@2"] = 1.5,
+      ["@4"] = 3.0,
+    },
   },
 }
