a9f4bb897112fa94656aeb9909b9870580051712
fakestarbaby <fakestarbaby@gmail.com>
2016-12-07 21:25:40 +0900

    Add system event listeneres

--- main.lua
+++ main.lua
@@ -1,3 +1,19 @@
 require "config.application"
 
+local Logger = require "lib.logger"
+local Composer = require "composer"
+
 display.setStatusBar(display.HiddenStatusBar)
+
+local function startApplication(event)
+end
+
+Runtime:addEventListener("system", function(event)
+  if event.type == "applicationStart" then
+    Logger.debug(event.type)
+
+    startApplication(event)
+  elseif event.type == "applicationExit" then
+    Logger.debug(event.type)
+  end
+end)
